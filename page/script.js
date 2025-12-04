42ce9cdc9d44b730b94150f241598178735245a6
Bezenemize <pilipenkoandrej52@gmail.com>
2025-12-04 12:49:29 +0300

    Add files via upload

---
+++ page/script.js
@@ -0,0 +1,8 @@
+document.getElementById('demoForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Спасибо за заявку! Мы свяжемся с вами в течение 15 минут для предоставления демо-доступа.');
+});
+
+console.log('Яндекс.Метрика: трекинг активирован');
+console.log('Google Analytics: трекинг активирован');
+console.log('CallTouch: сквозная аналитика активна');
