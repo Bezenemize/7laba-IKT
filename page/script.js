8626bebc92fba0865e8f30f39245c7078d80203b
Bezenemize <pilipenkoandrej52@gmail.com>
2025-12-11 16:09:22 +0300

    Update script.js

--- page/script.js
+++ page/script.js
@@ -3,6 +3,43 @@ document.getElementById('demoForm').addEventListener('submit', function(e) {
     alert('Спасибо за заявку! Мы свяжемся с вами в течение 15 минут для предоставления демо-доступа.');
 });
 
-console.log('Яндекс.Метрика: трекинг активирован');
-console.log('Google Analytics: трекинг активирован');
-console.log('CallTouch: сквозная аналитика активна');
+window.onload = function() {
+    var myForm = document.getElementById('demoForm');
+    myForm.onsubmit = function(event) {
+        event.preventDefault();
+        if (checkForm()) {
+            alert('Спасибо за заявку! Мы свяжемся с вами в течение 15 минут для предоставления демо-доступа.');
+            myForm.reset();
+        }
+    };
+    function checkForm() {
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+        var phone = document.getElementById('phone').value;
+        if (name === '') {
+            alert('Пожалуйста, введите ваше имя');
+            return false;
+        }
+        if (email === '') {
+            alert('Пожалуйста, введите email');
+            return false;
+        }
+        if (email.indexOf('@') === -1) {
+            alert('Email должен содержать @');
+            return false;
+        }
+        if (phone === '') {
+            alert('Пожалуйста, введите телефон');
+            return false;
+        }
+        return true;
+    }
+    var buttons = document.querySelectorAll('a[href="#demoForm"]');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].onclick = function(e) {
+            e.preventDefault();
+            var form = document.getElementById('demoForm');
+            form.scrollIntoView({behavior: 'smooth'});
+        };
+    }
+};
